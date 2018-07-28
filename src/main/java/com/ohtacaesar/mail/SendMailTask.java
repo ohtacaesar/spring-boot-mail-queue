442f0d572010e4df22c8290364fbdb1fe41688d0
ohtacaesar <ohtacaesar@gmail.com>
2018-07-29 03:11:33 +0900

    送信メールをログに表示する, テストメールを送信できるように

--- src/main/java/com/ohtacaesar/mail/SendMailTask.java
+++ src/main/java/com/ohtacaesar/mail/SendMailTask.java
@@ -7,27 +7,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 public class SendMailTask {
 
-
   @Autowired
   private MailService service;
-
-  @Autowired
-  private MailMessageEntityRepository repository;
-
 
   @Scheduled(fixedDelay = 3000)
   public void sendMail() {
     service.send();
-  }
-
-  @Scheduled(fixedDelay = 1000)
-  public void enqueueMail() {
-    MailMessageEntity msg = new MailMessageEntity();
-    msg.setFrom("ohtacaesar@gmail.com");
-    msg.setTo("ohtacaesar@gmail.com");
-    msg.setSubject("test");
-    msg.setText("test");
-    repository.save(msg);
   }
 
 }
