0f4cd989ddccaa6aa02c47f4cd50e46ebbcbe66c
ohtacaesar <ohtacaesar@gmail.com>
2018-07-29 01:07:17 +0900

    データベースからメールを取得して送信する

---
+++ src/main/java/com/ohtacaesar/mail/MailStatus.java
@@ -0,0 +1,6 @@
+package com.ohtacaesar.mail;
+
+public enum MailStatus {
+  NEW,
+  SENT,
+}
--- src/main/java/com/ohtacaesar/mail/SendMailTask.java
+++ src/main/java/com/ohtacaesar/mail/SendMailTask.java
@@ -2,27 +2,31 @@ package com.ohtacaesar.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.MailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 public class SendMailTask {
 
+
   @Autowired
-  private MailSender sender;
+  private MailService service;
 
   @Autowired
   private MailMessageEntityRepository repository;
 
-  @Scheduled(fixedDelay = 5000)
-  public void reportCurrentTime() {
+
+  @Scheduled(fixedDelay = 3000)
+  public void sendMail() {
+    service.send();
+  }
+
+  @Scheduled(fixedDelay = 1000)
+  public void enqueueMail() {
     MailMessageEntity msg = new MailMessageEntity();
     msg.setFrom("ohtacaesar@gmail.com");
     msg.setTo("ohtacaesar@gmail.com");
     msg.setSubject("test");
     msg.setText("test");
-    System.out.println(msg);
-
     repository.save(msg);
   }
 
