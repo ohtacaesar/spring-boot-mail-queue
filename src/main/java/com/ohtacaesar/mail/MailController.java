b081703119dae121d0e56dc122d1a209ce69f2b2
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2018-07-30 08:42:47 +0900

    メール送信フォーム

--- src/main/java/com/ohtacaesar/mail/MailController.java
+++ src/main/java/com/ohtacaesar/mail/MailController.java
@@ -1,12 +1,15 @@
 package com.ohtacaesar.mail;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,12 +24,14 @@ public class MailController {
   @GetMapping
   public ModelAndView index(
       ModelAndView mav,
+      MailMessageForm form,
       @PageableDefault(sort = "id", direction = Direction.DESC) Pageable pageable
   ) {
     Page<MailMessageEntity> page = repository.findAll(pageable);
 
     mav.addObject("page", page);
     mav.addObject("mails", page.getContent());
+    mav.addObject("form", form);
     mav.setViewName("index");
 
     return mav;
@@ -39,25 +44,22 @@ public class MailController {
     return e.getText();
   }
 
-
-  @PostMapping("send")
-  public String send() {
-    MailMessageEntity e = new MailMessageEntity();
-    e.setFrom("ohtacaesar@gmail.com");
-    e.setTo("ohtacaesar@gmail.com");
-    e.setSubject("テストメール");
-    e.setText("テキストメール\r\nテスト");
-    repository.save(e);
-
-    return "redirect:/";
-  }
-
   @PostMapping("{id}/resend")
   public String resend(@PathVariable int id) {
     MailMessageEntity e = repository.findOne(id);
     e = e.copy();
     e.setSubject("Copy from" + id);
     repository.save(e);
+
+    return "redirect:/";
+  }
+
+  @PostMapping("send")
+  public String create(@Validated @ModelAttribute MailMessageForm form) {
+    MailMessageEntity o = new MailMessageEntity();
+    BeanUtils.copyProperties(form, o);
+    o.setTo(form.getTo());
+    repository.save(o);
 
     return "redirect:/";
   }
@@ -75,4 +77,5 @@ public class MailController {
 
     return "redirect:/";
   }
+
 }
